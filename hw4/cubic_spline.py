dffadc8454a2ab7a8324c5d246689c34873235b3
Andy Lu <arlu@smu.edu>
2017-10-23 07:25:04 -0500

    First commit for this homework. Boiler plate stuff.

---
+++ hw4/cubic_spline.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Andy Lu
+SMU Mathematics
+MATH 5315 Fall 2017
+
+cubic_spline.py for Homework 4
+- This file defines two functions:
+  (1) cubic_spline_coefficients to calculate the cubic spline coefficients
+  (2) cubic_spline_evaluate to evaluate a cubic spline
+"""
+
+# Imports
+
+#################################################################################
+#                             Function Definitions                              #
+#################################################################################
+def cubic_spline_coefficients(t, y, alpha, beta):
+    
+    
+def cubic_spline_evaluate(t, y, z, x):
